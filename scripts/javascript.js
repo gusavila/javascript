33a8d29fb6871eb24bad560973c31e7a1b6e3a62
Gustavo Avila <gustavohenriqueavila10@gmail.com>
2022-05-05 16:57:04 -0300

    Ex01 completed
    Site criado para mudar de imagem e texto conforme o horário do seu computador

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,16 +1,21 @@
 var today = new Date();
 var hourNow = today.getHours();
 var greeting;
+var wallpaper;
 
 if (hourNow > 18) {
     greeting = 'Good evening!';
+    wallpaper = 'evening';
 } else if (hourNow > 12) {
     greeting = 'Good afternoon!';
+    wallpaper = 'afternoon';
 } else if (hourNow > 0) {
     greeting = 'Good morning!';
+    wallpaper = 'morning';
 } else {
     greeting = 'Welcome!';
+    wallpaper = 'road';
 }
 
 document.write('<h3>' + greeting + '</h3>');
-document.write('<img src="https://images5.alphacoders.com/463/thumb-1920-463801.jpg"');
+document.getElementById('body').style.background = 'URL(images/' + wallpaper + '.jpg) no-repeat center center fixed';
